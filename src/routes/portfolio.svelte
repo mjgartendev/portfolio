45cf4b9dded2411ad3f83ef113366fbbe8eafa6f
Michael Garten <mjgartendev@gmail.com>
2019-07-28 13:14:50 -0500

    Update portfolio.svelte

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -16,7 +16,7 @@
 </ul>
 <script>
 	let projects = [
-		{name: "Monaco Editor Playground", description: "Component IDE using Monaco Editor wrapped with Svelte 3, backed by Firestore realtime data", href:"https://fullstackvoyage.com"},
+		{name: "Monaco Editor Playground", description: "Component IDE using Monaco Editor wrapped with Svelte 3, backed by Firestore realtime data", href:"https://svelte-playground.web.app"},
 		{name: "Lit-Element Kanban-ish Board", description: "Some different list components and things using typescript and Lit-Element", href:"https://stackblitz.com/edit/lit-el-playground"},
 		{name: "Lit-Element App Layout Components", description: "Example using a few components to create a very dynamic, configurable app layout", href:"https://codepen.io/mjgartendev/pen/eoqWwV"},
 		{name: "Browser based IDE for bundling ES Modules (UI only)", description: "Layout only of my dream project. A reimagined, simplified, browser only development environment", href:"https://codepen.io/mjgartendev/full/zQOLqW"},
